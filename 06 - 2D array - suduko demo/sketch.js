373a42f04a1eef707ffb470029823a0d13dd661a
VictorWu03 <71724675+VictorWu03@users.noreply.github.com>
2020-10-02 09:47:36 -0600

    Sudoku text file compatibility
    10/2/2020

--- 06 - 2D array - suduko demo/sketch.js
+++ 06 - 2D array - suduko demo/sketch.js
@@ -1,32 +1,34 @@
 // sudoku demo - 2D array demo
 // 10/1/2020
 
-let sudoku = [[5,3,0,0,7,0,0,0,0],
-              [6,0,0,1,9,5,0,0,0],
-              [0,9,8,0,0,0,0,6,0],
-              [8,0,0,0,6,0,0,0,3],
-              [4,0,0,8,0,3,0,0,1],
-              [7,0,0,0,2,0,0,0,6],
-              [0,6,0,0,0,0,2,8,0],
-              [0,0,0,4,1,9,0,0,5],
-              [0,0,0,0,8,0,0,7,9]];
-
-let initialState = 
-              [[5,3,0,0,7,0,0,0,0],
-              [6,0,0,1,9,5,0,0,0],
-              [0,9,8,0,0,0,0,6,0],
-              [8,0,0,0,6,0,0,0,3],
-              [4,0,0,8,0,3,0,0,1],
-              [7,0,0,0,2,0,0,0,6],
-              [0,6,0,0,0,0,2,8,0],
-              [0,0,0,4,1,9,0,0,5],
-              [0,0,0,0,8,0,0,7,9]];
+let sudoku;
+let initialState;
 
 const GRIDESIZE = 9;
 let sideLength;
 
+function preload() {
+  sudoku = loadStrings("assets/2.txt");
+  initialState = loadStrings("assets/2.txt");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  //convert sudoku text file into 2d array
+  for (let i = 0; i < sudoku.length; i ++) {
+    sudoku[i] = sudoku[i].split(",");
+    initialState[i] = initialState[i].split(",");
+  }
+
+  //loop through array to change to num data type
+  for (let y = 0; y < GRIDESIZE; y ++) {
+    for(let x = 0; x < GRIDESIZE; x ++) {
+      sudoku[y][x] = int(sudoku[y][x]);
+      initialState[y][x] = int(initialState[y][x]);
+    }
+  }
+
 
   if (windowWidth < windowHeight) {
     sideLength = windowWidth/GRIDESIZE;
