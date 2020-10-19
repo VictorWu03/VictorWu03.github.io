ccc2a4ab1e4fe8438562f7639dea847b8f3a13fe
VictorWu03 <71724675+VictorWu03@users.noreply.github.com>
2020-10-18 19:14:16 -0600

    improving the detection of the board state score

--- Major Project - Gomoku - ai in progress/sketch.js
+++ Major Project - Gomoku - ai in progress/sketch.js
@@ -20,7 +20,9 @@ let winner;
 
 let whitePoints = 0;
 let blackPoints = 0;
-
+let score;
+let bestXMap = new Map();
+let bestYMap = new Map();
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -192,7 +194,7 @@ function mousePressed() {
     placeMarker(corX, corY);
     console.log(corX, corY);
   }
-  // evaluateBoardState();
+  evaluateBoardState();
 
 }
 
@@ -267,143 +269,273 @@ function checkWin() {
   }
 }
 
-// function evaluateBoardState() {
-//   //consecutive
-//   blackPoints = 0;
-//   whitePoints = 0;
-//   if (state === "play") {
-//     //horozontal
-//     for(let x = 0; x < board.length; x ++) {
-//       for (let y = 0; y < board.length; y ++) {
-//         if (board[y][x] === "white") {
-//           //horozontal
-//           if (x < board.length - 4) { //checking boundaries 
-//             if (board[y][x] === board[y][x + 1]) {
-//               if (board[y][x] === board[y][x + 2]) {
-//                 if (board[y][x] === board[y][x + 3]) {
-//                   if (board[y][x] === board[y][x + 4]) {
-//                     whitePoints += 100;
-//                   }
-//                   else{
-//                     whitePoints += 70;
-//                   }
-//                 }
-//                 else {
-//                   whitePoints += 30;
-//                 }
-//               }
-//               else {
-//                 whitePoints += 10;
-//               }
-//             }
-//             else {
-//               whitePoints += 0;
-//             }
-//           }
-//           // Vertical
-//           if (x < board.length - 4) { //checking boundaries 
-//             if (board[y][x] === board[y + 1][x]) {
-//               if (board[y][x] === board[y + 2][x]) {
-//                 if (board[y][x] === board[y + 3][x]) {
-//                   if (board[y][x] === board[y + 4][x]) {
-//                     whitePoints += 100;
-//                   }
-//                   else{
-//                     whitePoints += 70;
-//                   }
-//                 }
-//                 else {
-//                   whitePoints += 30;
-//                 }
-//               }
-//               else {
-//                 whitePoints += 10;
-//               }
-//             }
-//             else {
-//               whitePoints += 0;
-//             }
-//           }
-//           //diagonal down-right
-//           if (x < board.length - 4) { //checking boundaries 
-//             if (board[y][x] === board[y + 1][x + 1]) {
-//               if (board[y][x] === board[y + 2][x + 2]) {
-//                 if (board[y][x] === board[y + 3][x + 3]) {
-//                   if (board[y][x] === board[y + 4][x + 4]) {
-//                     whitePoints += 100;
-//                   }
-//                   else{
-//                     whitePoints += 70;
-//                   }
-//                 }
-//                 else {
-//                   whitePoints += 30;
-//                 }
-//               }
-//               else {
-//                 whitePoints += 10;
-//               }
-//             }
-//             else {
-//               whitePoints += 0;
-//             }
-//           }
-//           //diagonal up-right
-//           if (x < board.length - 4) { //checking boundaries 
-//             if (board[y][x] === board[y - 1][x + 1]) {
-//               if (board[y][x] === board[y - 2][x + 2]) {
-//                 if (board[y][x] === board[y - 3][x + 3]) {
-//                   if (board[y][x] === board[y - 4][x + 4]) {
-//                     whitePoints += 100;
-//                   }
-//                   else{
-//                     whitePoints += 70;
-//                   }
-//                 }
-//                 else {
-//                   whitePoints += 30;
-//                 }
-//               }
-//               else {
-//                 whitePoints += 10;
-//               }
-//             }
-//             else {
-//               whitePoints += 0;
-//             }
-//           }
-//         }
-//       }
-//     }
-//     console.log(whitePoints);
-//   }
-// }
+function evaluateBoardState() {
+  //consecutive
+  blackPoints = 0;
+  whitePoints = 0;
 
-function computerMove() {
-  if (state === "play" && turnState === "computer") {
+  if (state === "play") {
+    //horozontal
     for(let x = 0; x < board.length; x ++) {
       for (let y = 0; y < board.length; y ++) {
-        if (currentMove === "black") {
-          if (board[y][x] === null) {
-            board[y][x] = "B";
-            fill(currentMove);
-            circle(cellSize * x + centerPlayX + cellSize/2, cellSize * y + centerPlayY + cellSize/2, cellSize * 0.85);
-            currentMove = "white";
-            turnState = "human";
-            return board;
+        if (board[y][x] === "W") {
+          //horozontal
+          if (x < board.length - 4) { //checking boundaries 
+            if (board[y][x] === board[y][x + 1]) {
+              if (board[y][x] === board[y][x + 2]) {
+                if (board[y][x] === board[y][x + 3]) {
+                  if (board[y][x] === board[y][x + 4]) {
+                    whitePoints += 100;
+                  }
+                  else{
+                    whitePoints += 70;
+                  }
+                }
+                else {
+                  whitePoints += 30;
+                }
+              }
+              else {
+                whitePoints += 10;
+              }
+            }
+            else {
+              whitePoints += 5;
+            }
           }
-        }
-        else if (currentMove === "white") {
-          if (board[y][x] === null) {
-            board[y][x] = "W";
-            fill(currentMove);
-            circle(cellSize * x + centerPlayX + cellSize/2, cellSize * y + centerPlayY + cellSize/2, cellSize * 0.85);
-            currentMove = "black";
-            turnState = "human";
-            return board;
+          // Vertical
+          if (y < board.length - 4) { //checking boundaries 
+            if (board[y][x] === board[y + 1][x]) {
+              if (board[y][x] === board[y + 2][x]) {
+                if (board[y][x] === board[y + 3][x]) {
+                  if (board[y][x] === board[y + 4][x]) {
+                    whitePoints += 100;
+                  }
+                  else{
+                    whitePoints += 70;
+                  }
+                }
+                else {
+                  whitePoints += 30;
+                }
+              }
+              else {
+                whitePoints += 10;
+              }
+            }
+            else {
+              whitePoints += 5;
+            }
+          }
+          //diagonal down-right
+          if (y < board.length - 4 && x < board.length - 4) { //checking boundaries 
+            if (board[y][x] === board[y + 1][x + 1]) {
+              if (board[y][x] === board[y + 2][x + 2]) {
+                if (board[y][x] === board[y + 3][x + 3]) {
+                  if (board[y][x] === board[y + 4][x + 4]) {
+                    whitePoints += 100;
+                  }
+                  else{
+                    whitePoints += 70;
+                  }
+                }
+                else {
+                  whitePoints += 30;
+                }
+              }
+              else {
+                whitePoints += 10;
+              }
+            }
+            else {
+              whitePoints += 5;
+            }
+          }
+          //diagonal up-right
+          if (x < board.length - 4 && y > 3) { //checking boundaries 
+            if (board[y][x] === board[y - 1][x + 1]) {
+              if (board[y][x] === board[y - 2][x + 2]) {
+                if (board[y][x] === board[y - 3][x + 3]) {
+                  if (board[y][x] === board[y - 4][x + 4]) {
+                    whitePoints += 100;
+                  }
+                  else{
+                    whitePoints += 70;
+                  }
+                }
+                else {
+                  whitePoints += 30;
+                }
+              }
+              else {
+                whitePoints += 10;
+              }
+            }
+            else {
+              whitePoints += 5;
+            }
           }
         }
       }
     }
+
+
+    for(let x = 0; x < board.length; x ++) {
+      for (let y = 0; y < board.length; y ++) {
+        if (board[y][x] === "B") {
+          //horozontal
+          if (x < board.length - 4) { //checking boundaries 
+            if (board[y][x] === board[y][x + 1]) {
+              if (board[y][x] === board[y][x + 2]) {
+                if (board[y][x] === board[y][x + 3]) {
+                  if (board[y][x] === board[y][x + 4]) {
+                    blackPoints -= 100;
+                  }
+                  else{
+                    blackPoints -= 70;
+                  }
+                }
+                else {
+                  blackPoints -= 30;
+                }
+              }
+              else {
+                blackPoints -= 10;
+              }
+            }
+            else {
+              blackPoints -= 5;
+            }
+          }
+          // Vertical
+          if (y < board.length - 4) { //checking boundaries 
+            if (board[y][x] === board[y + 1][x]) {
+              if (board[y][x] === board[y + 2][x]) {
+                if (board[y][x] === board[y + 3][x]) {
+                  if (board[y][x] === board[y + 4][x]) {
+                    blackPoints -= 100;
+                  }
+                  else{
+                    blackPoints -= 70;
+                  }
+                }
+                else {
+                  blackPoints -= 30;
+                }
+              }
+              else {
+                blackPoints -= 10;
+              }
+            }
+            else {
+              blackPoints -= 5;
+            }
+          }
+          //diagonal down-right
+          if (y < board.length - 4 && x < board.length - 4) { //checking boundaries 
+            if (board[y][x] === board[y + 1][x + 1]) {
+              if (board[y][x] === board[y + 2][x + 2]) {
+                if (board[y][x] === board[y + 3][x + 3]) {
+                  if (board[y][x] === board[y + 4][x + 4]) {
+                    blackPoints -= 100;
+                  }
+                  else{
+                    blackPoints -= 70;
+                  }
+                }
+                else {
+                  blackPoints -= 30;
+                }
+              }
+              else {
+                blackPoints -= 10;
+              }
+            }
+            else {
+              blackPoints -= 5;
+            }
+          }
+          //diagonal up-right
+          if (x < board.length - 4 && y > 3) { //checking boundaries 
+            if (board[y][x] === board[y - 1][x + 1]) {
+              if (board[y][x] === board[y - 2][x + 2]) {
+                if (board[y][x] === board[y - 3][x + 3]) {
+                  if (board[y][x] === board[y - 4][x + 4]) {
+                    blackPoints -= 100;
+                  }
+                  else{
+                    blackPoints -= 70;
+                  }
+                }
+                else {
+                  blackPoints -= 30;
+                }
+              }
+              else {
+                blackPoints -= 10;
+              }
+            }
+            else {
+              blackPoints -= 5;
+            }
+          }
+        }
+      }
+    }
+    console.log(whitePoints);
+    console.log(blackPoints);
+    score = whitePoints + blackPoints;
+    return score;
+  }
+}
+
+function bestMove() {
+  let highscore = 0;
+
+  for(let x = 0; x < board.length; x ++) {
+    for (let y = 0; y < board.length; y ++) {
+      if(board[y][x] === null) {
+        board[y][x] = "W";
+        evaluateBoardState();
+        if (score > highscore) {
+          bestXMap.set('x', x);
+          bestYMap.set('y', y);
+          highscore = score;
+        }
+        board[y][x] = null;
+      }
+    }
+  }
+}
+
+function computerMove() {
+  if (state === "play" && turnState === "computer") {
+
+    bestMove();
+    let bestX = bestXMap.get('x');
+    let bestY = bestYMap.get('y');
+
+    if (currentMove === "black") {
+
+      board[bestY][bestX] = "B";
+      fill(currentMove);
+      circle(cellSize * bestX + centerPlayX + cellSize/2, cellSize * bestY + centerPlayY + cellSize/2, cellSize * 0.85);
+      currentMove = "white";
+      turnState = "human";
+      return board;
+
+    }
+    else if (currentMove === "white") {
+
+      board[bestY][bestX] = "W";
+      fill(currentMove);
+      circle(cellSize * bestX + centerPlayX + cellSize/2, cellSize * bestY + centerPlayY + cellSize/2, cellSize * 0.85);
+      currentMove = "black";
+      turnState = "human";
+      return board;
+
+    }
+    //   }
+    // }
   }
 }
